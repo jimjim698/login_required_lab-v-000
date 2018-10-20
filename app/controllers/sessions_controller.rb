dd82bbf36d07113b9ddee5a395e16a00d3190406
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-20 23:38:23 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
-  
+  before_action
   
   def new 
   end 
   
   def create 
   end 
+  
+  
+  
+  private
+  
+  def require_login
+    
   
  
 end
