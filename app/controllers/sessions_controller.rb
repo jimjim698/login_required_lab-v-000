f8a1d4a1c81ab7f2e34a43a72e0445391a3e69d4
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-20 23:49:19 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,9 +12,7 @@ class SessionsController < ApplicationController
   private
   
   def require_login
-    if !current_user 
-      redirect_to login_path 
-    end 
+      redirect_to login_path unless current_user
   end 
     
   
